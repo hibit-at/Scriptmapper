73d8129fb9ce8388a42fc30b6b1ea7aaf031c5c7
hibit-at <hibit_at@homail.com>
2021-09-04 10:22:16 +0900

    fov

--- utils.py
+++ utils.py
@@ -1,15 +1,23 @@
 from copy import deepcopy
+from random import random as rd
 
 template = {
     "ActiveInPauseMenu": True,
+    "TurnToHeadUseCameraSetting": False,
     "Movements": [
         {
             "StartPos": {
                 "x": 0,
                 "y": 0,
-                "z": 0
+                "z": 0,
+                "FOV": 60
             },
             "StartRot": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+            },
+            "StartHeadOffset": {
                 "x": 0,
                 "y": 0,
                 "z": 0
@@ -17,24 +25,36 @@ template = {
             "EndPos": {
                 "x": 0,
                 "y": 0,
-                "z": 0
+                "z": 0,
+                "FOV": 60
             },
             "EndRot": {
                 "x": 0,
                 "y": 0,
                 "z": 0
             },
-            "Duration": 0,
+            "EndHeadOffset": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+            },
+            "TurnToHead": False,
+            "Duration": 4,
             "Delay": 0,
             "EaseTransition": False
         }
     ]
 }
 
-def create_template():
+
+def create_template(isHead=False, height=1.5):
     move_tmp = deepcopy(template['Movements'][0])
-    move_tmp['EaseTransition'] = False
+    move_tmp['TurnToHead'] = isHead
+    offset = height-1.5
+    move_tmp['StartHeadOffset'] = {'x':0,'y':offset,'z':0}
+    move_tmp['EndHeadOffset'] =  {'x':0,'y':offset,'z':0}
     return move_tmp
+
 
 def grid_parse(mode, origin, dummyend_grid):
     ans = []
@@ -51,15 +71,18 @@ def grid_parse(mode, origin, dummyend_grid):
                 log_texts.append('！fill にパラメータが指定されていません。')
                 continue
             param = eval(text.split(',')[0][4:])
-            log_texts.append('fill を検出')
-            log_texts.append(str(param))
-            text_pattern = ','.join(text.split(',')[1:])
+            log_texts.append(f'fill を検出　パラメータ　{str(param)}')
+            orig_text_pattern = ','.join(text.split(',')[1:])
             span = param
             end_grid = origin[i+1]['time']
-            log_texts.append(f'スクリプト {text_pattern} をグリッド {start_grid} から {end_grid} まで{span}の間隔で敷き詰めます。')
+            log_texts.append(
+                f'スクリプト {orig_text_pattern} をグリッド {start_grid} から {end_grid} まで {span} の間隔で敷き詰めます。')
             cnt = 0
             current_grid = start_grid
             while current_grid < end_grid:
+                # "!"と'?'を変数に
+                text_pattern = orig_text_pattern.replace('!', str(cnt))
+                text_pattern = text_pattern.replace('?',str(rd()))
                 cnt += 1
                 ans.append({'time': current_grid, 'text': text_pattern})
                 log_texts.append(f'{current_grid} : {text_pattern}')
@@ -70,13 +93,13 @@ def grid_parse(mode, origin, dummyend_grid):
                 log_texts.append('！copy にパラメータが指定されていません。')
                 continue
             param = eval(text.split(',')[0][4:])
-            log_texts.append('copy を検出')
-            log_texts.append(str(param))
+            log_texts.append(f'copy を検出　パラメータ　{str(param)}')
             text_pattern = ','.join(text.split(',')[1:])
             end_grid = origin[i+1]['time']
             t_start_grid = param
             t_end_grid = param + end_grid - start_grid
-            log_texts.append(f'グリッド{start_grid}～{end_grid}のスクリプトを、グリッド{t_start_grid}～{t_end_grid}からコピーします')
+            log_texts.append(
+                f'グリッド {start_grid}～{end_grid} のスクリプトを、グリッド {t_start_grid}～{t_end_grid} からコピーします')
             tmp = deepcopy(ans)
             for t in tmp:
                 t_grid = t['time']
