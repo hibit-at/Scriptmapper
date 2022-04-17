debf48ab2e51ec953c5b179f3ddb799ac8bb4459
rei05 <hasegawa0kn@gmail.com>
2022-04-17 18:36:51 +0900

    add rotate command param

--- LongCommandsUtils.py
+++ LongCommandsUtils.py
@@ -6,24 +6,42 @@ from BasicElements import Pos, Rot, Line
 
 def rotate(self, text, dur):
     # def_value
-    r = 3
-    h = 3
+    r1 = 3
+    h1 = 3
     a = 1
     o = 1.0
-    s = 0
+    s1 = 0
+    j = 0
+    w = 360
     if len(text) > 6:
         param = [eval(i) for i in text[6:].split(',')]
         if len(param) > 0:
-            r = param[0]
+            r1 = param[0]
         if len(param) > 1:
-            h = param[1]
+            h1 = param[1]
         if len(param) > 2:
             a = param[2]
         if len(param) > 3:
             o = param[3]
         if len(param) > 4:
-            s = param[4]
-    self.logger.log(f'パラメータ r:{r} h:{h} a:{a} o:{o} s:{s}')
+            s1 = param[4]
+        if len(param) > 5:
+            j = param[5]
+        if len(param) > 6:
+            w = param[6]
+        if len(param) > 7:
+            r2 = param[7]
+        else:
+            r2 = r1
+        if len(param) > 8:
+            h2 = param[8]
+        else:
+            h2 = h1
+        if len(param) > 9:
+            s2 = param[9]
+        else:
+            s2 = s1
+    self.logger.log(f'パラメータ r1:{r1} h1:{h1} a:{a} o:{o} s1:{s1} j:{j} w:{w} r2:{r2} h2:{h2} s2:{s2}')
     span = max(1/30, dur/36)
     spans = []
     while dur > 0:
@@ -34,8 +52,11 @@ def rotate(self, text, dur):
     for i in range(span_size):
         new_line = Line(spans[i])
         new_line.visibleDict = deepcopy(self.visibleObject.state)
-        theta = 2*pi*i/span_size - 1/2*pi
-        theta2 = 2*pi*(i+1)/span_size - 1/2*pi
+        theta = 2*pi*i*(w/360)/span_size - 1/2*pi + j*2*pi/360
+        next_theta = 2*pi*(i+1)*(w/360)/span_size - 1/2*pi + j*2*pi/360
+        r = r1 + (r2-r1)*i/span_size
+        h = h1 + (h2-h1)*i/span_size
+        s = s1 + (s2-s1)*i/span_size
         angle = atan2(h-a, r)
         px = round(r*cos(theta), 3)
         pz = round(r*sin(theta)+o, 3)
@@ -44,10 +65,14 @@ def rotate(self, text, dur):
         new_line.start.pos = Pos(px, h, pz)
         new_line.start.rot = Rot(rx, ry, s)
         new_line.start.fov = self.fov
-        px = round(r*cos(theta2), 3)
-        pz = round(r*sin(theta2)+o, 3)
+        r = r1 + (r2-r1)*(i+1)/span_size
+        h = h1 + (h2-h1)*(i+1)/span_size
+        s = s1 + (s2-s1)*(i+1)/span_size
+        angle = atan2(h-a, r)
+        px = round(r*cos(next_theta), 3)
+        pz = round(r*sin(next_theta)+o, 3)
         rx = degrees(angle)
-        ry = -degrees(theta2)+270
+        ry = -degrees(next_theta)+270
         new_line.end.pos = Pos(px, h, pz)
         new_line.end.rot = Rot(rx, ry, s)
         new_line.end.fov = self.fov
